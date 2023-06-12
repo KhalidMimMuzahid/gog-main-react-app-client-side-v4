60700a4d86657d32e68780210fc44ca655f7ddb1
sojib-sudo <mdsojibmandol@gmail.com>
2023-06-12 18:55:23 +0600

    our upcumming betch design

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import SchoolChamp from "./UpCommingBatches/SchoolChamp/SchoolChamp";
 
 const Home = () => {
   return (
     <div>
       <h1>landing page</h1>
+      <SchoolChamp/>
     </div>
   );
 };
---
+++ src/Pages/Home/UpCommingBatches/SchoolChamp/SchoolChamp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SchoolChamp = () => {
+    return (
+        <div>
+            This is our school champ
+        </div>
+    );
+};
+
+export default SchoolChamp;
