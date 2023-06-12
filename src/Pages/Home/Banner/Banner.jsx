4c4a53044e281df686c40440e00f76256a701fb8
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-12 19:03:53 +0600

    ...

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,78 +1,82 @@
 import React from "react";
 import cap from "../../../assets/Banner/cap.svg";
+import hiring from "../../../assets/Banner/hiring.svg";
+import meeting from "../../../assets/Banner/meeting.svg";
+import placement from "../../../assets/Banner/placement.svg";
+import roadmap from "../../../assets/Banner/roadmap.svg";
 
 const Banner = () => {
   return (
     <div className='p-4 grid grid-cols-1 md:grid-cols-2'>
       <div className='flex items-center justify-center gap-[10px]'>
         <div className='flex flex-col gap-[40px]'>
-          <div className='w-[340px] h-[170px] rounded-[20px] bg-[#BDF5BC]'>
+          <div className='w-[340px] h-[170px] rounded-[20px] bg-[#BDF5BC] p-5 hover:translate-x-8 hover:translate-y-8 transition-[1500ms] hover:cursor-pointer'>
             <div className='flex'>
-              <div className="w-1/2">
-                <p>
+              <div className="w-1/2 font-medium">
+                <p className="text-[20px]">
                   Students Currently
                   <br />
                   Enrolled
                 </p>
-                <p>200+</p>
+                <p className="text-[36px]">200+</p>
               </div>
               <div className="w-1/2">
                 <img src={cap} alt='enrolled-student' />
               </div>
             </div>
           </div>
-          <div className='w-[340px] h-[170px] rounded-[20px] bg-[#BDF5BC]'>
+          <div className='w-[340px] h-[170px] rounded-[20px] bg-[#BDF5BC] p-5'>
             <div className='flex'>
-              <div className="w-1/2">
-                <p>Total Batch</p>
-                <p>10+</p>
+              <div className="w-1/2 font-medium">
+                <p className="text-[20px]">Total Batch</p>
+                <p className="text-[36px]">10+</p>
               </div>
               <div className="w-1/2">
-                <img src={cap} alt='enrolled-student' />
+                <img src={hiring} alt='enrolled-student' />
               </div>
             </div>
           </div>
-          <div className='w-[340px] h-[170px] rounded-[20px] bg-[#BDF5BC]'>
+          <div className='w-[340px] h-[170px] rounded-[20px] bg-[#BDF5BC] p-5'>
             <div className='flex'>
-              <div>
-                <p>
+              <div className="w-1/2 font-medium">
+                <p className="text-[20px]">
                   We Started <br />
                   Our Journey
                 </p>
-                <p>2023</p>
+                <p className="text-[36px]">2023</p>
               </div>
-              <div>
-                <img src={cap} alt='enrolled-student' />
+              <div className="w-1/2">
+                <img src={meeting} alt='enrolled-student' />
               </div>
             </div>
           </div>
         </div>
         <div className='flex flex-col gap-[40px]'>
-          <div className='w-[340px] h-[170px] rounded-[20px] bg-[#BDF5BC]'>
+          <div className='w-[340px] h-[170px] rounded-[20px] bg-[#BDF5BC] p-5'>
             <div className='flex'>
-              <div>
-                <p>
+              <div className="w-1/2 font-medium">
+                <p className="text-[20px]">
                   Placement <br />
                   Rate
                 </p>
-                <p>96%</p>
+                <p className="text-[36px]">96%</p>
               </div>
-              <div>
-                <img src={cap} alt='enrolled-student' />
+              <div className="w-1/2">
+                <img src={placement} alt='enrolled-student' />
               </div>
             </div>
           </div>
-          <div className='w-[340px] h-[170px] rounded-[20px] bg-[#BDF5BC]'>
+          <div className='w-[340px] h-[170px] rounded-[20px] bg-[#BDF5BC] p-5'>
             <div className='flex'>
-              <div>
-                <p>
+              <div className="w-1/2 font-medium">
+                <p className="text-[20px]">
                   Hiring <br />
                   Partners
                 </p>
-                <p>50+</p>
+                <p className="text-[36px]">50+</p>
               </div>
-              <div>
-                <img src={cap} alt='enrolled-student' />
+              <div className="w-1/2">
+                <img src={roadmap} alt='enrolled-student' />
               </div>
             </div>
           </div>
