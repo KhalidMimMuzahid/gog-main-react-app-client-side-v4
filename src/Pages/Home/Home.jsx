441a1fd7a98a42f9ae6079fed38290a89016a974
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-13 14:26:07 +0600

    super30 card made

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,13 +4,15 @@ import Banner from "./Banner/Banner";
 
 import SchoolChamp from "./UpCommingBatches/SchoolChamp/SchoolChamp";
 import Landing from "./Landing/Landing";
+import Super30 from "./Super30/Super30";
 
 const Home = () => {
   return (
     <div>
 
       <Landing />
-      <Banner/>
+      <Banner />
+      <Super30/>
       <SchoolChamp/>
 
     </div>
