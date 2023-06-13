ffba71fbeda2a1cf4b3d44a58808411860363a72
JAYENDRA06 <jayendra.awasthi2020@vitstudent.ac.in>
2023-06-13 10:57:49 +0530

    ok

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import Banner from "./Banner/Banner";
 
 import SchoolChamp from "./UpCommingBatches/SchoolChamp/SchoolChamp";
+import Landing from "./Landing/Landing";
 
 const Home = () => {
   return (
     <div>
 
+      <Landing />
       <Banner/>
-
-      <h1>landing page</h1>
       <SchoolChamp/>
 
     </div>
