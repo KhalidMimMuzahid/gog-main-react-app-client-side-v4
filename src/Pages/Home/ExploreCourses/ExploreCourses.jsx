0822373698b0e04bb0481a87814aad850f284e9e
sojib-sudo <mdsojibmandol@gmail.com>
2023-06-14 13:50:54 +0600

    Design: Explore courses

---
+++ src/Pages/Home/ExploreCourses/ExploreCourses.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExploreCourses = () => {
+    return (
+        <div className='p-[65px]'>
+            <h2>This is Explore courses</h2>
+        </div>
+    );
+};
+
+export default ExploreCourses;
