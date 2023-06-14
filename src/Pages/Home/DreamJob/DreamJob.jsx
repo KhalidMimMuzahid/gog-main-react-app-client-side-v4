4b44b06d99537041dbc6ad3426f7091ca6fd0259
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-15 00:22:39 +0600

    Design: DreamJob components

--- src/Pages/Home/DreamJob/DreamJob.jsx
+++ src/Pages/Home/DreamJob/DreamJob.jsx
@@ -7,7 +7,7 @@ const DreamJob = () => {
             <h2 className=' md:px-[60px] font-poppins  font-[600] text-[22px] md:text-[48px]  md:leading-[72px] dark:text-[#FFFFFF] text-[#101010]'>Secure your Dream Job <br /> with these <span className='text-[#4BA25D] dark:text-[#37ED81]'>5 Simple Steps</span> </h2>
             <div className='md:px-[60px] grid grid-cols-1 gap-10 md:grid-cols-2 mt-[40px] '>
                 <div>
-                    <ul className='text-[#101010] dark:text-[#FFFFFF]'>
+                    <ul className='text-[#101010] font-[500] md:text-[24px] text-[18px] dark:text-[#FFFFFF]'>
                         <li className='md:p-[20px] p-[10px] md:mb-[40px] mb-[20px]'>1. Register</li>
                         <li className='md:p-[20px] p-[10px] md:my-[40px] my-[20px]'>2. Start the Program</li>
                         <li className='md:p-[20px] p-[10px] md:my-[40px] my-[20px]'>3. Study with Us</li>
