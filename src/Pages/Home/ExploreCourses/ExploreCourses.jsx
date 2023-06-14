0dede6a3f28f06260cf7028201b335e2a1ea95e3
sojib-sudo <mdsojibmandol@gmail.com>
2023-06-14 16:12:22 +0600

    ok

--- src/Pages/Home/ExploreCourses/ExploreCourses.jsx
+++ src/Pages/Home/ExploreCourses/ExploreCourses.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import backgroundImage from "../../../assets/someBG/01.svg";
 
 const ExploreCourses = () => {
-    return (
-        <div className='p-[65px]'>
-            <h2>This is Explore courses</h2>
+  return (
+    <div
+      className=" border h-64 sm:h-96 relative m-[65px]"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className=" text-2xl ">
+        <div className="flex justify-between items-center">
+          <div className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+            provident?
+          </div>
+          <div>button</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ExploreCourses;
