9333ece2d619c6b311ef160080dd8b3b9e644907
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-14 17:12:56 +0530

    add: studentReview component

---
+++ src/Pages/Home/StudentReview/StudentReview.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function StudentReview() {
+  return (
+    <div className="p-[65px]">
+      <div className="text-[#101010] dark:text-white">
+        <h2 className="text-4xl font-medium text-center text-transparent bg-clip-text text-gradient bg-gradient-to-r from-[#37ED81] via-[#3DA5BD] to-[#905BEC] ">
+          100+ CODING GEEKS TO INSPIRE YOU
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default StudentReview;
