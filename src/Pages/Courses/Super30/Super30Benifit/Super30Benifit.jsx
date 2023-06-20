7a6b3d5ed685a2966699699ee199e25499d4efbf
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-20 14:29:45 +0600

    Benefits of Super-30 Job Guarantee batch added

---
+++ src/Pages/Courses/Super30/Super30Benifit/Super30Benifit.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Super30Benifit = () => {
+  return (
+    <div className='w-[90vw] mx-auto rounded-[40px] my-20 mt-10 bg-[#E1FFE8] dark:bg-slate-950 p-5'>
+      <div className='my-10'>
+        <h1 className='text-center text-[24px] md:text-[48px] font-semibold dark:text-white'>
+          Benefits of <span className='text-[#37ED81]'>Super-30</span> Job
+          Guarantee batch
+        </h1>
+        <div className='my-6 flex items-center justify-center'>
+          <div className="text-left text-[18px] font-normal dark:text-white">
+          <p>1. 100% Placement guarantee for Super-30 students.</p>
+          <p>2. Six months internship certificate powered by Geeks of Gurukul.</p>
+          <p>3. Course completion certificate by Skill India.</p>
+          <p>4. Letter of Recommendation (LOR) powered by Geeks of Gurukul.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Super30Benifit;
