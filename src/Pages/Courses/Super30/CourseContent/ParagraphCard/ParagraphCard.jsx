121f785cbf998e8abb0275ee3b810be6c2d00ced
sojib-sudo <mdsojibmandol@gmail.com>
2023-06-20 13:22:36 +0600

    Design: s3o-CoursesContent components

--- src/Pages/Courses/Super30/CourseContent/ParagraphCard/ParagraphCard.jsx
+++ src/Pages/Courses/Super30/CourseContent/ParagraphCard/ParagraphCard.jsx
@@ -4,7 +4,7 @@ const ParagraphCard = ({slb}) => {
     const {heading, subHeading} = slb;
     return (
         <>
-            <p> <span className='text-[#101010] text-[700] '>{heading}</span> </p>
+            <p className='mb-[16px] dark:text-[white] text-[#101010]'> <span className=' dark:text-[white] text-[#101010] font-[600] md:text-[16px] text-[15px]'>{heading} </span>: {subHeading} </p>
         </>
     );
 };
