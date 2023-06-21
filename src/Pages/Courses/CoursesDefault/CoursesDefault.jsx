56f3ce06f03d2ba04c196e0051d829db5c5fed7e
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-21 10:40:52 +0530

    fix: coursesHero responsiveness

--- src/Pages/Courses/CoursesDefault/CoursesDefault.jsx
+++ src/Pages/Courses/CoursesDefault/CoursesDefault.jsx
@@ -3,7 +3,7 @@ import CoursesHero from "./CoursesHero/CoursesHero";
 
 const CoursesDefault = () => {
   return (
-    <div className="p-[30px] md:p-[115px] dark:text-white">
+    <div className="p-[30px] md:px-[115px] md:py-[65px] dark:text-white">
       <CoursesHero />
     </div>
   );
--- src/Pages/Courses/CoursesDefault/CoursesHero/CoursesHero.jsx
+++ src/Pages/Courses/CoursesDefault/CoursesHero/CoursesHero.jsx
@@ -4,7 +4,7 @@ import HeroFormComponent from './HeroFormComponent/HeroFormComponent'
 
 function CoursesHero() {
   return (
-    <div className='grid lg:grid-cols-12 grid-cols-1 gap-10'>
+    <div className='grid lg:grid-cols-12 grid-cols-1 gap-10 md:gap-14'>
         <HeroInfoComponent />
         <HeroFormComponent />
     </div>
