510911e8afaeed34a7a7efc1de68d17a30d87745
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-22 16:40:29 +0530

    fix: UI of Home page

--- src/Pages/Home/LearningJourneyCard/LearningJourneyCard.jsx
+++ src/Pages/Home/LearningJourneyCard/LearningJourneyCard.jsx
@@ -6,7 +6,7 @@ import polygon from "../../../assets/someBG/polygon.svg";
 
 function LearningJourneyCard() {
   return (
-    <div className="relative rounded-3xl my-[70px] p-10 lg:p-16 text-[#101010] dark:text-white bg-white shadow-md shadow-[#19B85140] dark:bg-[#1C202A] dark:shadow-none">
+    <div className="relative rounded-3xl my-[70px] p-6 md:p-10 lg:p-16 text-[#101010] dark:text-white bg-white shadow-md shadow-[#19B85140] dark:bg-[#1C202A] dark:shadow-none">
       <img
         src={star}
         className="w-[20%] md:w-[15%] lg:w-[10%] absolute left-0 bottom-0"
@@ -23,10 +23,10 @@ function LearningJourneyCard() {
         src={circle}
         className="w-[20%] md:w-[15%] lg:w-[10%] absolute top-0 right-0"
       />
-      <div className="">
+      <div className="text-center lg:text-left">
         <div className="grid grid-cols-1 lg:grid-cols-12 items-center gap-10">
           <div className="col-span-1 lg:col-span-10 z-10">
-            <p className="text-xl md:text-4xl lg:text-5xl font-medium pb-10 leading-relaxed">
+            <p className="text-xl md:text-4xl lg:text-5xl font-medium pb-6 md:pb-10 leading-relaxed">
               "Reach Out and Ignite{" "}
               <span className="text-[#00A3FF] leading-relaxed">
                 Your Learning Journey
@@ -41,7 +41,7 @@ function LearningJourneyCard() {
           <div className="col-span-1 lg:col-span-2 flex justify-center lg:justify-end w-full z-10">
             <button
               type="button"
-              className="text-sm md:text-xl rounded-full bg-[#00A3FF] text-white py-2 px-4"
+              className="text-sm md:text-xl rounded-full bg-[#00A3FF] text-white py-2 px-4 w-full lg:w-auto hover:bg-gradient-to-r hover:from-[#00A3FF] hover:to-[#905BEC]"
             >
               Contact us
             </button>
