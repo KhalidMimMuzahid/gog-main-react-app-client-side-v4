ad57a88cc3ab5993360d9db7930a4bcad553c674
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-25 23:43:05 +0600

    design: about us page

---
+++ src/Pages/AboutUs/Heading/Heading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <div className='mt-[64px] px-[20px] md:px-[30px] lg:px-[65px] '>
+            <h1 className=' md:text-[48px] text-[24px] text-center font-[600] text-[#101010] dark:text-[white]'><span className='text-[#37ED81] '>"Empowering Minds,</span> Unleashing Potential"</h1>
+            <p className='text-center text-[#101010] dark:text-[white] md:text-[20px] text-[15px]'>"Revolutionizing Education through Innovative Technology and Personalized Learning Experiences"</p>
+        </div>
+    );
+};
+
+export default Heading;
