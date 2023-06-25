387a4993822c554b622291256fd83eea9319276a
Sarwarhridoy4 <ssarwarhridoy4@gmail.com>
2023-06-25 17:23:00 +0600

    trusted company done

---
+++ src/Pages/Alumni/StatsCard/EachCard/EachCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const EachCard = ({ card }) => {
+    const {img,body} = card
+  return (
+    <div className='flex gap-[24px] flex-col justify-center items-center w-[180px] '>
+    <div className='md:p-[10px] dark:bg-[#1A211F] bg-[#E1FFE8] rounded-[100%]'>
+        <img className='w-[40px]' src={img} alt="Spets Images" />
+    </div>
+    <div className=' h-[54px] text-center'>
+        <p className='md:px-[10px] px-[10px] dark:text-white'>{body}</p>
+    </div>
+</div>
+  )
+}
+
+export default EachCard
