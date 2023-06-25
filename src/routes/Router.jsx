05789bde208d771b83c941acca3890069cc08f8a
Sarwarhridoy4 <ssarwarhridoy4@gmail.com>
2023-06-25 14:38:56 +0600

    alumni hero done

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -14,6 +14,7 @@ import SignIn from "../Pages/SignIn/SignIn";
 import ContactUs from "../Pages/ContactUs/ContactUs";
 import AboutUs from "../Pages/AboutUs/AboutUs";
 import ChampsOfBasicCoding from "../Pages/Courses/SpecificCourses/ChampsOfBasicCoding";
+import Alumni from "../Pages/Alumni/Alumni";
 
 const router = createBrowserRouter([
   {
@@ -50,6 +51,10 @@ const router = createBrowserRouter([
       {
         path: "/hire-from-us",
         element: <HireFromUs />,
+      },
+      {
+        path: "/alumni",
+        element: <Alumni/>
       },
       {
         path: "/contact-us",
