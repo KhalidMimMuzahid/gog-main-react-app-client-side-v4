dc357ba84bc70e6551e841f1e39d5041d28058cd
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-25 17:23:00 +0600

    trusted company done

--- src/Pages/Alumni/Alumni.jsx
+++ src/Pages/Alumni/Alumni.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from './Hero/Hero'
+import StatsCard from './StatsCard/StatsCard'
+import TrustedCompany from './TrustedCompany/TrustedCompany'
 
 const Alumni = () => {
   return (
       <div className='p-[64px]'>
-          <Hero/>
+      <Hero />
+      <StatsCard />
+      <TrustedCompany/>
       </div>
       
   )
