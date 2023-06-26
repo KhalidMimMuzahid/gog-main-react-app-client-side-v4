d892dce6e2292ac2a0998e2deebd3befdda83ee3
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-26 11:26:23 +0600

    code merged

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Heading from "./Heading/Heading";
 import AboutUsInfo from "./AboutUsInfo/AboutUsInfo";
-import OurMission from "./OurMission/OurMission";
-import OurCalture from "./OurCalture /OurCalture";
+import MessagefromCEO from "./MessagefromCEO/MessagefromCEO";
+
 import StudentPlaced from "../Home/StudentPlaced/StudentPlaced";
 import Record from "./Record/Record";
+import OurMission from "./OurMission/OurMission";
 
 const AboutUs = () => {
   return (
     <div>
       <Heading />
       <AboutUsInfo />
-      <OurMission />
+      <MessagefromCEO/>
       <div className="mt-[64px] mb-[24px] bg-[#D1FFD0] dark:bg-[#1A2C27] rounded-tl-[20px] rounded-[20px]">
-        <OurCalture />
+        <OurMission />
         <Record />
         <div className="py-[120px]">
           <StudentPlaced showHeading={true} />
