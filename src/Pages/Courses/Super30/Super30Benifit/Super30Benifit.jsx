d4d10efd2c9b67e1659430de1bd4dbe220aeb590
Sarwarhridoy4 <ssarwarhridoy4@gmail.com>
2023-06-28 10:06:31 +0600

    few changes done

--- src/Pages/Courses/Super30/Super30Benifit/Super30Benifit.jsx
+++ src/Pages/Courses/Super30/Super30Benifit/Super30Benifit.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Super30Benifit = () => {
   return (
-    <div className='w-[90vw] mx-auto rounded-[40px] my-20 mt-10 bg-[#E1FFE8] dark:bg-slate-950 p-5'>
+    <div className='w-[90vw] mx-auto rounded-[40px] my-20 mt-10 bg-[#E1FFE8] dark:bg-[#1C1F29] p-5'>
       <div className='my-10'>
         <h1 className='text-center text-[22px] md:text-[48px] font-semibold dark:text-white'>
           Benefits of <span className='text-[#37ED81]'>Super-30</span> Job
           Guarantee batch
         </h1>
         <div className='my-6 flex items-center justify-center'>
-          <div className="text-left text-[18px] font-normal dark:text-white">
+          <div className="text-left text-[14px] md:text-[18px] font-normal dark:text-white">
           <p>1. 100% Placement guarantee for Super-30 students.</p>
           <p>2. Six months internship certificate powered by Geeks of Gurukul.</p>
           <p>3. Course completion certificate by Skill India.</p>
