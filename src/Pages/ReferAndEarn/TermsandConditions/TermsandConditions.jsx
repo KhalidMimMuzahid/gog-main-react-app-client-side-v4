b4cf0c8b9007d91ce9140608a8fe6d14c7b99e23
Khalid Mim Muzahid <khalidmimm@gmail.com>
2023-06-28 11:46:37 +0600

    ok

--- src/Pages/ReferAndEarn/TermsandConditions/TermsandConditions.jsx
+++ src/Pages/ReferAndEarn/TermsandConditions/TermsandConditions.jsx
@@ -4,6 +4,10 @@ const TermsandConditions = () => {
     return (
         <div className='mt-[80px]'>
             <h1 className="font-[600] text-[24px] md:text-[36px] text-center bg-clip-text text-transparent bg-gradient-to-r from-green-500 via-blue-500 to-purple-500">Terms and Conditions</h1>
+            <div>
+                
+            </div>
+
         </div>
     );
 };
